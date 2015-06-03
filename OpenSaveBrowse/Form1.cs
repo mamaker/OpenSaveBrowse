bc3e9de7dfb470c8ecaba0ae5ce7ae1f8584f3a8
mamaker <madhuvasudevan@hotmail.com>
2015-06-02 20:43:54 -0500

    Done

--- OpenSaveBrowse/Form1.cs
+++ OpenSaveBrowse/Form1.cs
@@ -12,6 +12,7 @@ namespace OpenSaveBrowse
 {
     public partial class Form1 : Form
     {
+        const string workFolder = @"C:\MyOwnApps\GitLoc\HeadFirstCsharp\WorkFolder\";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +20,7 @@ namespace OpenSaveBrowse
 
         private void button1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.InitialDirectory = @"c:\MyFolder\Default\";
+            openFileDialog1.InitialDirectory = workFolder;
             openFileDialog1.Filter = "Text Files (*.txt)|*.txt|"
             + "Comma-Delimited Files (*.csv)|*.csv|All Files (*.*)|*.*";
             openFileDialog1.FileName = "default_file.txt";
@@ -32,9 +33,42 @@ namespace OpenSaveBrowse
             }
         }
 
-        private void OpenSomeFile(string p)
+        private void OpenSomeFile(string filename)
         {
-            MessageBox.Show("Open File: ") ;
+            MessageBox.Show("Opened File: "+filename) ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = workFolder;
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|"
+            + "Comma-Delimited Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SaveTheFile(saveFileDialog1.FileName);
+            }
+        }
+
+        private void SaveTheFile(string filename)
+        {
+            MessageBox.Show("Saved File: " + filename);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = workFolder;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                BrowseTheFile(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void BrowseTheFile(string foldername)
+        {
+            MessageBox.Show("Browsed Folder: " + foldername);
         }
     }
 }
